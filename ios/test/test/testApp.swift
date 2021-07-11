1b1bf567cb5f01ab1a281e14df1fec6af1345ff5
wildaces215 <test@test.com>
2021-07-11 19:19:41 -0400

    hi

---
+++ ios/test/test/testApp.swift
@@ -0,0 +1,20 @@
+//
+//  testApp.swift
+//  test
+//
+//  Created by Craig Albertson on 7/11/21.
+//
+
+import SwiftUI
+
+@main
+struct testApp: App {
+    let persistenceController = PersistenceController.shared
+
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+                .environment(\.managedObjectContext, persistenceController.container.viewContext)
+        }
+    }
+}
